cb48c5091e19d23244cbedcd8f112811cae4a9b3
mayo2021 <mayoriramirez23@gmail.com>
2022-03-09 14:11:16 -0400

    pesos y dolares

---
+++ dolares y pesos.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int pesos;
+  int dolares;
+  int conversion;
+  
+  printf("Ingrese el valor en pesos: \n");
+  scanf("%d" , &pesos);
+
+  printf("Igrese el valor en dolares: \n");
+  scanf("%d" , &dolares);
+
+ conversion =  dolares * 58;
+
+
+  if (dolares  > pesos)
+{
+printf("%d dolares es mayor a pesos");
+}   
+
+else if (pesos > conversion)
+{
+printf("pesos es mayor que dólares \n ");
+printf("%d dolares son  pesos", "&dolares %d");
+
+}   
+  
+else if (pesos == conversion)
+{
+printf("%d cual las dos monedas son iguales\n");
+printf("%d dolares son pesos", "&dolares %d");
+  {
+  
+  }
+
+}
+
+      
+}
+
+
+  
